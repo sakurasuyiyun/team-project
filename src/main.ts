b1c97457666d5ea2ffb01bf7d4e89a56e40717b8
NOW <2503071753@qq.com>
2023-09-01 17:02:39 +0800

    配置element

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,24 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+// element 组件
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import 'dayjs/locale/zh-cn'
 // @ts-ignore
 import App from '@/App.vue'
 import router from './router'
 
-const app = createApp(App)
 
+const app = createApp(App)
+app.use(ElementPlus, {
+  locale: zhCn,
+})
 app.use(createPinia())
 app.use(router)
-
+app.use(ElementPlus)
 app.mount('#app')
+
+
+
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -7,27 +7,27 @@ interface SaleMap {
 	__typename: string;
 }
 
-interface SalePie {
-	first_cid: number;
-	sale_today: string;
-	sale_yesterday: string;
-	sale_three_days: string;
-	sale_seven_days: string;
-	sale_fourteen_days: string;
-	sale_thirty_days: string;
-	sale_sixty_days: string;
-	today_money: string;
-	yesterday_money: string;
-	three_days_money: string;
-	seven_days_money: string;
-	fourteen_days_money: string;
-	thirty_days_money: string;
-	sixty_days_money: string;
-	name: string;
-}
+// interface SalePie {
+// 	first_cid: number;
+// 	sale_today: string;
+// 	sale_yesterday: string;
+// 	sale_three_days: string;
+// 	sale_seven_days: string;
+// 	sale_fourteen_days: string;
+// 	sale_thirty_days: string;
+// 	sale_sixty_days: string;
+// 	today_money: string;
+// 	yesterday_money: string;
+// 	three_days_money: string;
+// 	seven_days_money: string;
+// 	fourteen_days_money: string;
+// 	thirty_days_money: string;
+// 	sixty_days_money: string;
+// 	name: string;
+// }
 
 interface IHomeData {
-	salePie: SalePie[];
+	// salePie: SalePie[];
 	saleMap: SaleMap[];
 }
 
@@ -35,13 +35,13 @@ import {ref, onMounted, watch, reactive} from "vue";
 import axios from "axios";
 
 const data = reactive<IHomeData>({
-	salePie: [],
+	// salePie: [],
 	saleMap: []
 })
 
 axios.get('http://kumanxuan1.f3322.net:8360/admin/stat').then(res => {
 	data.saleMap = res.data.data.saleMap;
-	data.salePie = res.data.data.salePie;
+	// data.salePie = res.data.data.salePie;
 })
 </script>
 
