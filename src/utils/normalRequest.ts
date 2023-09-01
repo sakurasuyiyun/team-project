fa9229e0efe723d4d577063af93cf5ab6c1a727b
yiyun-su <71871466+sakurasuyiyun@users.noreply.github.com>
2023-09-01 23:55:26 +0800

    优化了axios的二次封装

--- src/utils/normalRequest.ts
+++ src/utils/normalRequest.ts
@@ -1,8 +1,32 @@
 import Axios from 'axios'
 
 const instance = Axios.create({
-    baseURL: 'http://111.230.242.195/',
+    baseURL: 'http://111.230.242.195',
     timeout: 10000
 })
+
+export const get = (url: string, params = {}) => {
+    return new Promise((resolve, reject) => {
+        instance.get(url, {params}).then((response) => {
+            resolve(response.data)
+        }, err => {
+            reject(err)
+        })
+    })
+}
+
+export const post = (url: string, data = {}) => {
+    return new Promise((resolve, reject) => {
+        instance.post(url, data, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then((response) => {
+            resolve(response.data)
+        }, err => {
+            reject(err)
+        })
+    })
+}
 
 export default instance
