89c9a7dcf9e08ee30de87b381a7f371a6ef8bc23
yiyun-su <71871466+sakurasuyiyun@users.noreply.github.com>
2023-09-02 10:13:44 +0800

    新增首页数据获取接口

--- src/api/HomePageApi.ts
+++ src/api/HomePageApi.ts
@@ -1,3 +1,8 @@
 import http from "@/utils/HomePageRequest"
+import {get} from "@/utils/normalRequest"
+
+// 获取城市销量数据
 export const getData = (): RSP<IHomeData> => http.get('/admin/stat')
 
+//  获取销售饼图数据
+export const getSalePie = (): RQ<Api,Login> => get('/api/getSalePie',{})
