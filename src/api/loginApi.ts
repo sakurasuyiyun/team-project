d5816f1ef79160a9f6391a1b17c4404ff17604d2
yiyun-su <71871466+sakurasuyiyun@users.noreply.github.com>
2023-09-02 17:35:38 +0800

    删除注册接口

--- src/api/loginApi.ts
+++ src/api/loginApi.ts
@@ -4,8 +4,3 @@ export const login = ():RQ<Api,Login> => get('/api/login',{
     username: 'admin',
     password: '12345678'
 })
-
-export const register = ():RQ<Api,Register> => post('/api/register',{
-    username: 'admin',
-    password: '12345678'
-})
