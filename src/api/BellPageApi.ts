43b11bd814cb8ab58e8feecdcc24a4d9a012750a
CZW22heartburst <89078948+CZW22heartburst@users.noreply.github.com>
2023-09-06 19:14:31 +0800

    9.6项目进度

--- src/api/BellPageApi.ts
+++ src/api/BellPageApi.ts
@@ -1,5 +1,10 @@
 
-import {get,post} from "@/utils/normalRequest";
+import { get, post } from "@/utils/normalRequest";
 
 // 获取秒杀活动列表
-export const getFKList = (): RQ<Api,Login> => get('/api/getSeckillList')
+export const getFKList = (): RQ<Api, Login> => get('/api/getSeckillList')
+
+// 添加秒杀活动列表
+export const addFKList = (token:String,title:String,activeState:Number,activeStartTime:String,activeEndTime:String,isActive:Number): RQ<Api, Login> => post('/api/addSeckillList', {
+  token: token,
+})
